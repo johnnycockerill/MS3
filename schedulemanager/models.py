bcb7485bb84f5758e40ffd5cccafaac5c6b068b6
johnnycockerill <167190345+johnnycockerill@users.noreply.github.com>
2025-04-29 19:35:45 +0000

    create models.py, populate with schema for service type and vehicle type

---
+++ schedulemanager/models.py
@@ -0,0 +1,27 @@
+from schedulemanager import db
+
+
+class Service(db.Model):
+    # schema for the service model
+    id = db.Column(db.Integer, primary_key=True)
+    service_name = db.Column(db.String(40), unique=True, nullable=False)
+    vehicles = db.relationship("Vehicle", backref="service", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represet itself in the form of a string
+        return self.category.name
+
+class Vehicle(db.Model):
+    # schema for the vehicle model
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_reg = db.Column(db.String(50), unique=True, nullable=False)
+    vehicle_type = db.Column(db.String(50), nullable=False)
+    work_completed = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("service.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represet itself in the form of a string
+        return "Reg: {0} - Type: {1} | Due: {2} - Completed: {3}".format(
+            self.vehicle_reg, self.vehicle_type, self.due_date, self.work_completed
+        )
