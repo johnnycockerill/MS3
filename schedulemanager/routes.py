22013811574fddff5ecbd0d12a8f65d7dbeae96e
johnnycockerill <167190345+johnnycockerill@users.noreply.github.com>
2025-04-29 21:20:42 +0000

    create route for adding new service type

--- schedulemanager/routes.py
+++ schedulemanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from schedulemanager import app, db
 from schedulemanager.models import Service, Vehicle
 
@@ -15,4 +15,9 @@ def services():
 
 @app.route("/add_service_type", methods=["GET", "POST"])
 def add_service_type():
+    if request.method == "POST":
+        service = Service(service_name=request.form.get("service_name"))
+        db.session.add(service)
+        db.session.commit()
+        return redirect(url_for("services"))
     return render_template("add_service.html")
