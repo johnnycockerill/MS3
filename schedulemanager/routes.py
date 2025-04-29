4211a78173e84da48f71cfaaff8c7dc3fd64d1e0
johnnycockerill <167190345+johnnycockerill@users.noreply.github.com>
2025-04-29 20:58:45 +0000

    create services.html and add_service.html, create basic routes for these pages

--- schedulemanager/routes.py
+++ schedulemanager/routes.py
@@ -6,3 +6,13 @@ from schedulemanager.models import Service, Vehicle
 @app.route("/")
 def home():
     return render_template("vehicles.html")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+@app.route("/add_service_type", methods=["GET", "POST"])
+def add_service_type():
+    return render_template("add_service.html")
