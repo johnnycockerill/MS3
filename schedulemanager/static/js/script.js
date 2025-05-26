1f8cd39d30aae5be3ffd8473a8091bb2f4f03cb1
johnnycockerill <jt13be6r@students.codeinstitute.net>
2025-05-26 13:30:12 +0100

    remove collapsible footer element, tweak css to become responsive

--- schedulemanager/static/js/script.js
+++ schedulemanager/static/js/script.js
@@ -18,19 +18,3 @@ document.addEventListener('DOMContentLoaded', function() {
     let collapsible = document.querySelectorAll('.collapsible');
     M.Collapsible.init(collapsible);
   });
-
-   const coll = document.getElementsByClassName('collapsible');
-   let i;
-
-   for (i = 0; i < coll.length; i++) {
-       coll[i].addEventListener('click', function() {
-           this.classList.toggle('active');
-           var content = this.nextElementSibling;
-
-           if (content.style.display === 'block' || content.style.display === 'flex') {
-               content.style.display = 'none';
-           } else {
-               content.style.display = 'block';
-           }
-       });
-   }
